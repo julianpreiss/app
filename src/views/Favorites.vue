e58db0639e588560c0b93bb416fb5ff16103023b
Julian Preiss <julianpreiss23@gmail.com>
2022-02-21 01:12:46 -0300

    [Favorites] logica para traer favs de cada user

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -11,7 +11,7 @@
         <v-icon>mdi-chevron-left</v-icon>
       </v-btn>
       <h2 class="my-2 titulos text-center mx-auto">Mis favoritos</h2>
-      <!--<v-flex
+      <v-flex
         xs12 
         md6
         v-for="room in rooms"
@@ -20,116 +20,145 @@
         
         <v-card
           class="mx-auto my-3"
-          :to="{ name: 'DetailRoom', params: { id: room._id } }"
         >
-          <div class="d-flex flex-no-wrap justify-space-between">
-            <div>
-              <v-card-title
-                class="text-h5"
-                v-text="room.name"
-              ></v-card-title>
-
-                <v-card-subtitle>
-                  <p class="mb-2">
-                    <v-icon>mdi-map-marker-outline</v-icon>
-                    {{room.address}}
-                  </p>
-                  <p class="price mb-2">
-                    ${{room.price}}
-                  </p>
-                </v-card-subtitle>
-                
-                <v-card-text>
-                  <v-rating
-                    :value="room.stars"
-                    color="pink"
-                    dense
-                    half-increments
-                    size="14"
-                  >
-                  </v-rating>
-                </v-card-text>
-              </div>
-
-              <v-avatar
-                class="ma-3"
-                size="140"
-                tile
+          <v-img 
+            height="250" 
+            :src="'http://localhost:8001/api/img/' + room.img"
+          />
+          <v-card-title class="order">
+            {{ room.name }}
+            <v-btn
+              icon
+              @click="deleteFavorite(room.idFav)"
+            >
+              <v-icon
+                color="#ff7dd8"
               >
-                <v-img :src="room.img"></v-img>
-              </v-avatar>
-            </div>
+                mdi-heart
+              </v-icon>
+            </v-btn>
+          </v-card-title>
+
+          <v-card-text>
+            <v-row class="mx-0 order">
+              <v-rating
+                :value="room.stars"
+                color="pink"
+                dense
+                half-increments
+                readonly
+                size="14"
+              ></v-rating>
+              <p class="grey--text ms-4">{{ room.stars }} (413)</p>
+              <p class="price">$ {{ room.price }}</p>
+            </v-row>
+            <v-card-title class="text-subtitle-1 pl-0 pt-3 pb-2">
+              <p class="mb-2">
+                <v-icon>mdi-map-marker-outline</v-icon>
+                {{ room.address }}
+              </p>
+            </v-card-title>
             <div>
-              <v-card-actions
-                class="space action" 
-                block
-              >
-              <v-btn
-                class="btn-call"
-                color="white" 
-                text 
-                block
-                id="room.id"
-                :to="{ name: 'BookingProcess', params: { id: room._id} }"
-              >Reservar</v-btn>
-              </v-card-actions>
+              <p>{{ room.description }}</p>
             </div>
-          </v-card>
-      </v-flex>-->
+          </v-card-text>
+          <v-card-actions class="space action" block>
+            <v-btn
+              class="btn-call"
+              color="white"
+              text
+              block
+              id="room.id"
+              :to="{ name: 'BookingProcess', params: { id: room._id} }"
+            >
+              Reservar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
   import API from '../config.js'
+  import axios from 'axios'
   export default {
     data() {
       return {
         rooms: [],
-        baseUrl: API + '/rooms', // 'http://localhost:8001/api/rooms' //llamara a la api en un archivo js
+        baseUrl: API + '/rooms/id?id=',
+        favoritesIds: {},
       };
     },
     created(){
-      this.getRooms();
+      this.checkFavorite ();
     },
     methods:{
-      async getRooms(){
-        const res = await this.axios.get( this.baseUrl ) //Repasar el tallar para ver si podemos cambiar axios por otra conexión
-        this.rooms = res.data
+      async checkFavorite () {
+        const favoritesUrl = API + '/favorites/id?id=' + sessionStorage.id
+        const res = await this.axios.get( favoritesUrl )
+        if (res.data.length !== 0) {
+          this.favorite = true
+          this.favoritesIds = res.data
+          for (const fav of this.favoritesIds) {
+          this.getFavorite(fav.roomid, fav._id)
+          }
+        } else {
+          this.favorite = false
+          this.favoriteid = null
+        }
+      },
+      async getFavorite(id, idFav){
+        const res = await this.axios.get( this.baseUrl + id )
+        console.log(this.rooms)
+        res.data.idFav = idFav
+        this.rooms.push(res.data)
+        console.log(this.rooms)
+      },
+      deleteFavorite (id) {
+        const url = API + '/favorites/id?id=' + id
+        axios.delete( url ).then((res) => {
+          this.rooms = []
+          this.checkFavorite()
+          console.log('res', res)
+        })
       }
     }
   }
 </script>
 
 <style>
-  .sb{
-      margin-bottom: -1rem;
-  }
-  .space{
-    justify-content: space-around;
-  }
-  .back{
-    position: absolute;
-    margin-top: 0.5rem;
-    justify-content: flex-start;
-  }
-  .action{
-    background-color: #EA1F54;
-    color: white;
-  }
-  .price{
-    font-size: larger;
-    font-weight: bolder;
-    color: #051A26;
-  }
-  .back{
-    position: absolute;
-    margin-top: 0.5rem;
-    justify-content: flex-start;
-  }
-  .btn-call{
-    letter-spacing: normal;
-    text-transform: capitalize;
-    font-size: 1.2rem !important;
-  }
+.order {
+  justify-content: space-between;
+}
+.space {
+  justify-content: space-around;
+}
+.action {
+  background-color: #ea1f54;
+  color: white;
+}
+.price {
+  font-size: larger;
+  font-weight: bolder;
+  color: #051a26;
+}
+.btn-call {
+  letter-spacing: normal;
+  text-transform: capitalize;
+  font-size: 1.2rem;
+}
+.nav-room {
+  text-align-last: center;
+}
+.center {
+  text-align: center;
+}
+.left {
+  float: left;
+}
+.right {
+  float: right;
+}
 </style>
