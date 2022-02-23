3c9df7dd5e64b1fff92ee89ec45aeffc78625fb1
Julian Preiss <julianpreiss23@gmail.com>
2022-02-23 01:17:33 -0300

    [Bookings] reorganiza codigo

--- src/views/Bookings.vue
+++ src/views/Bookings.vue
@@ -11,66 +11,69 @@
         <v-icon>mdi-chevron-left</v-icon>
       </v-btn>
       <h2 class="my-2 titulos text-center mx-auto">Mis reservas</h2>
-      <v-flex
+      <!--<p class="price mb-2" v-if="this.sesion == false">
+        Debes iniciar sesión para ver tus favoritos
+      </p>-->
+      <v-flex 
         xs12 
         md6
         v-for="room in rooms"
         :key="room.id"
       >
-          <v-card
-            class="mx-auto my-3"
-          >
-            <div class="d-flex flex-no-wrap justify-space-between">
-              <div>
-                <v-card-title
-                  class="text-h5"
-                  v-text="room.name"
-                ></v-card-title>
-
-                <v-card-subtitle>
-                  <p class="mb-2">
-                    <v-icon>mdi-map-marker-outline</v-icon>
-                    {{room.address}}, {{room.district}}.
-                  </p>
-                  <p class="price mb-2">
-                    {{ room.date }}
-                  </p>
-                  <p class="price mb-2">
-                    {{ room.time }}
-                  </p>
-                </v-card-subtitle>
-              </div>
-
-              <v-avatar
-                class="ma-3"
-                size="140"
-                tile
-              >
-                <v-img :src="'http://localhost:8001/api/img/' + room.img" />
-              </v-avatar>
-            </div>
+        <v-card
+          class="mx-auto my-3"
+        >
+          <div class="d-flex flex-no-wrap justify-space-between">
             <div>
-              <v-card-actions
-                class="space action" 
+              <v-card-title
+                class="text-h5"
+                v-text="room.name"
+              ></v-card-title>
+
+              <v-card-subtitle>
+                <p class="mb-2">
+                  <v-icon>mdi-map-marker-outline</v-icon>
+                  {{room.address}}, {{room.district}}.
+                </p>
+                <p class="price mb-2">
+                  {{ room.date }}
+                </p>
+                <p class="price mb-2">
+                  {{ room.time }}
+                </p>
+              </v-card-subtitle>
+            </div>
+
+            <v-avatar
+              class="ma-3"
+              size="140"
+              tile
+            >
+              <v-img :src="'http://localhost:8001/api/img/' + room.img" />
+            </v-avatar>
+          </div>
+          <div>
+            <v-card-actions
+              class="space action" 
+              block
+            >
+              <div
+                color="accent-4" 
+                text 
                 block
               >
-                <div
-                  color="accent-4" 
-                  text 
-                  block
-                >
-                  <v-rating 
-                      :value="room.stars" 
-                      background-color="white lighten-3"
-                      color="white"
-                      half-increments 
-                      hover
-                      size="25"
-                  ></v-rating>
-                </div>
-              </v-card-actions>
-            </div>
-          </v-card>
+                <v-rating v-if="room.date < date"
+                    :value="room.stars" 
+                    background-color="white lighten-3"
+                    color="white"
+                    half-increments 
+                    hover
+                    size="25"
+                ></v-rating>
+              </div>
+            </v-card-actions>
+          </div>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -82,7 +85,10 @@
     data() {
       return {
         rooms: [],
-        id: 0,
+        id: null,
+        date: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
+        .toISOString()
+        .substr(0, 10),
       };
     },
   created(){
